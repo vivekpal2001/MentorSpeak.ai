fe4e215472a27080139990647ba537f35304ebad
Vivek_Kumar/1901 <kumarvivekmailme@gmail.com>
2025-07-21 01:54:41 +0530

    different-meeting-variants

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -4,16 +4,18 @@ import Image from "next/image";
 
 interface Props{
     title: string,
-    description: string
+    description: string,
+    image?: string;
 }
 
 export const EmptyState = ({
     title,
-    description
+    description,
+    image = "/empty.svg"
 }: Props) => {
     return (
         <div className=" flex flex-col items-center justify-center">
-                <Image src="/empty.svg" alt="empty" height={240} width={240} />
+                <Image src={image} alt="empty" height={240} width={240} />
                 <div className="flex flex-col gap-y-6 max-w-md mx-auto text-center">
                     <h6 className="text-lg font-medium text-center">{title}</h6>
                     <p className="text-sm text-muted-foreground">{description}</p>
--- src/modules/meetings/ui/views/meeting-id-views.tsx
+++ src/modules/meetings/ui/views/meeting-id-views.tsx
@@ -8,6 +8,10 @@ import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-q
 import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
 import { useState } from "react";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props{
     meetingId: string;
@@ -47,6 +51,12 @@ export const MeetingIdView = ({meetingId}: Props)=>{
         await removeMeeting.mutateAsync({ id: meetingId});
     };
 
+    const isActive = data.status === "active";
+    const isUpcoming = data.status === "upcoming";
+    const isCancelled = data.status === "cancelled";
+    const isCompleted = data.status === "completed";
+    const isProcessing = data.status === "processing";
+
     return (
         <>
             <RemoveConfirmation />
@@ -55,14 +65,24 @@ export const MeetingIdView = ({meetingId}: Props)=>{
                 onOpenChange={setUpdateMeetingDialogOpen}
                 initialValues={data}
             />
-            <div className="flex-1 py-4">
+            <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
                 <MeetingIdViewHeader
                     meetingId={meetingId}
                     meetingName={data.name}
                     onEdit={()=>setUpdateMeetingDialogOpen(true)}
                     onRemove={handleRemoveAgent}
                 />
-                 {JSON.stringify(data,null,2)}
+                 {isCancelled && ( <CancelledState/> )}
+                 {isProcessing && ( <ProcessingState/> )}
+                 {isCompleted && <div>Completed</div>}
+                 {isActive && (<ActiveState meetingId={meetingId}/>)}
+                 {isUpcoming && (
+                    <UpcomingState
+                        meetingId={meetingId}
+                        onCancelMeeting={() => {}}
+                        isCancelling={false}
+                    />
+                 )}
             </div>
         </>
     )
