93580be3da2195a4810ee2abbb1bd04347e0ac66
Vivek_Kumar/1901 <kumarvivekmailme@gmail.com>
2025-07-20 11:50:47 +0530

    meeting-datatable

--- src/modules/agents/types.ts
+++ src/modules/agents/types.ts
@@ -1,4 +1,5 @@
 import { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "@/trpc/routers/_app"
 
+export type AgentsGetMany = inferRouterOutputs<AppRouter>["agents"]["getMany"]["items"]
 export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"]
--- src/modules/meetings/ui/views/meeting-views.tsx
+++ src/modules/meetings/ui/views/meeting-views.tsx
@@ -1,18 +1,28 @@
 "use client"
 
+import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client"
-import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const  MeetingsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
     return (
-        <div>
-            {JSON.stringify(data)}
+        <div className="flex-1 pb-4 px-4 md:px:8 flex flex-col gap-y-4">
+            <DataTable data={data.items} columns={columns}/>
+            {data.items.length === 0 && (
+                <EmptyState
+                    title="Create your first meeting"
+                    description="Schedule a meeting to connect with others. Each meeting lets you collaborate, share ideas, and interact with participants in the real time"
+                />
+            )}
         </div>
+        
     )
 }
 
